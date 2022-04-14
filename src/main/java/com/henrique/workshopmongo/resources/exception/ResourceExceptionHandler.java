5a8e546aa7df7330396007a95ea1c35032d2ae98
henrique1023 <henrique.amorim@fatec.sp.gov.br>
2022-04-14 14:04:59 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/henrique/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.henrique.workshopmongo.resources.exception;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.henrique.workshopmongo.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	//essa anotação faz o metodo capturar o erro que for lançado
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);	
+	}
+	
+//	@ExceptionHandler(DatabaseException.class)
+//	public ResponseEntity<StandardError> dataBase(DatabaseException e, HttpServletRequest request){
+//		String error = "Database error";
+//		HttpStatus status = HttpStatus.BAD_REQUEST;
+//		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+//		return ResponseEntity.status(status).body(err);	
+//	}
+}
--- src/main/java/com/henrique/workshopmongo/services/UserService.java
+++ src/main/java/com/henrique/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.henrique.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.henrique.workshopmongo.domain.User;
 import com.henrique.workshopmongo.repository.UserRepository;
+import com.henrique.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
 	}
 }
