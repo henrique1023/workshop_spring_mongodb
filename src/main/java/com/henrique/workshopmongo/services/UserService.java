54060d1b9e81674bde9ceceb0070bcf3c776f697
henrique1023 <henrique.amorim@fatec.sp.gov.br>
2022-04-14 14:16:17 -0300

    Inserção de usuário com POST

--- src/main/java/com/henrique/workshopmongo/services/UserService.java
+++ src/main/java/com/henrique/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.henrique.workshopmongo.domain.User;
+import com.henrique.workshopmongo.dto.UserDTO;
 import com.henrique.workshopmongo.repository.UserRepository;
 import com.henrique.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objdto) {
+		return new User(objdto.getId(),objdto.getName(),objdto.getEmail());
 	}
 }
