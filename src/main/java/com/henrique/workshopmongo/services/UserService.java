ac9bfc380d0897027202048808cc89a1ee58f9f5
henrique1023 <henrique.amorim@fatec.sp.gov.br>
2022-04-18 14:12:40 -0300

    Atualização de usuário com PUT

--- src/main/java/com/henrique/workshopmongo/services/UserService.java
+++ src/main/java/com/henrique/workshopmongo/services/UserService.java
@@ -13,28 +13,45 @@ import com.henrique.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.insert(obj);
 	}
-	
+
 	public User fromDTO(UserDTO objdto) {
-		return new User(objdto.getId(),objdto.getName(),objdto.getEmail());
+		return new User(objdto.getId(), objdto.getName(), objdto.getEmail());
 	}
-	
+
 	public void deleteById(String id) {
 		repository.deleteById(id);
+	}
+
+	public User update(User obj) {
+		Optional<User> newObj = repository.findById(obj.getId());
+		User u = updateData(newObj, obj);
+		return repository.save(u);
+	}
+
+	private User updateData(Optional<User> newObj, User obj) {
+		if (newObj.isPresent()) {
+			newObj.get().setEmail(obj.getEmail());
+			newObj.get().setName(obj.getName());
+			User user = newObj.get();
+			return user;
+		}
+		throw new ObjectNotFoundException("Objeto não existe no banco");
+		
 	}
 }
